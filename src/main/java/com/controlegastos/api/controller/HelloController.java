c6dce5f4d212eb6a66173b7f4d8e04140937cb88
Adriel <adrielisaias02@gmail.com>
2026-08-27 15:47:42 -0300

    refactor: organiza HelloController

--- src/main/java/com/controlegastos/api/controller/HelloController.java
+++ src/main/java/com/controlegastos/api/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.controlegastos.api;
+package com.controlegastos.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
